036ab0d4a2c81e2639c41838ac62509d1da31d93
J-eld <70251176+J-eld@users.noreply.github.com>
2021-09-05 23:46:35 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react'
+import LoadingIcon from './assets/loadingIcon.svg'
+import clsx from 'clsx'
 
 function App() {
-  return (
+
+  const [weatherData, setWeatherData] = useState('');
+  const [description, setDescription] = useState('');
+  const [logo, setLogo] = useState('');
+  const [units, setUnits] = useState(true);
+  const [noLocation, setNoLocation] = useState(false);
+
+  let lon = ''
+  let lat = ''
+
+  function changeUnits() {
+    setWeatherData('')
+    setUnits(!units)
+  }
+
+  navigator.permissions.query({ name: 'geolocation' })
+    .then(res => {
+      if (res.state === 'granted') {
+        setNoLocation(true)
+      } else {
+        setNoLocation(false)
+      }
+    })
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      lon = pos.coords.longitude;
+      lat = pos.coords.latitude;
+      
+
+      axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=59388f8beea76e951bbadf379c246a23&units=${units ? 'metric' : 'imperial'}`)
+      .then((res) => {
+        setWeatherData(res.data)
+        setDescription(res.data.weather[0].description)
+        setLogo(res.data.weather[0].icon)
+      })
+      .catch((err) => console.log(err))
+    })
+  }, [units])
+
+  if (noLocation) return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={clsx({['hidden']: !weatherData})}>
+        <div className="city-name">
+          {weatherData ? weatherData.name : 'q'}
+        </div>
+        <div className="dateTime">
+          {new Date().toDateString() + '\xa0\xa0' + new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})}
+        </div>
+        <div className="temperature">
+          {weatherData ? weatherData && Math.round(weatherData.main.temp) + '\xB0' + (units ? 'C' : 'F') : 'q'}
+        </div>
+        <img src={`http://openweathermap.org/img/wn/${logo}@2x.png`} />
+        <div className="weather-description">
+          {description.charAt(0).toUpperCase() + description.slice(1)}
+        </div>
+      </div>
+      <label className="switch">
+        <input value={units} checked={units} onChange={changeUnits} type="checkbox" />
+        <span className="slider round"><span className={clsx({['CelsiusText']: units, ['FahrenheitText']: !units})}>{units ? '\xB0C' : '\xB0F'}</span></span>
+      </label>
+      <div className={clsx({['loading']: !weatherData})}></div>
     </div>
   );
+
+  return (<div className="App" >Please Enable Location Permissions</div>)
 }
 
 export default App;
